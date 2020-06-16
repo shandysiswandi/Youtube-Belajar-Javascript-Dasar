b1f63f319d6e10a4a521ad68c6bbf621cf298ab3
shandysiswandi <shandysiswandi@gmail.com>
2020-06-17 00:53:52 +0700

    add variables

---
+++ variables.js
@@ -0,0 +1,60 @@
+/** Aturan 1:
+ *  Semua `variable` JavaScript harus diidentifikasi(identified) dengan nama yang unik.
+ *  Nama-nama unik ini disebut pengidentifikasi(identifiers).
+ */
+var name = "sandy";
+var name = "siswandi";
+
+/** Aturan 2:
+ * Pengidentifikasi dapat berupa nama pendek (seperti x dan y)
+ * Tapi lebih bagus dan disarankan menggunakan nama yang lebih deskriptif (seperti usia, jumlah, nama).
+ */
+var myAge = 22;
+var x = 1;
+var y = 2;
+
+/** Aturan 3:
+ * Lebih bagus lagi menggunakan bahasa inggris, karena:
+ *
+ * 1. untuk alasan propesional
+ * 2. sekaligus belajar bahasa inggris
+ * 3. hampir semua yang berhubungan dengan komputer ada bahasa inggris nya.
+ */
+
+var nama = "budi";
+var nani = "Naruto";
+
+/** Aturan 4:
+ * Aturan umum untuk membuat nama untuk variable (pengidentifikasi unik) adalah:
+ *
+ * 1. Nama variable dapat berisi huruf, angka, garis bawah (_), dan tanda dolar ($).
+ * 2. Nama variable harus dimulai dengan huruf.
+ * 3. Nama variable juga dapat dimulai dengan $ dan _ (karena dalam JavaScript $ dan _ dianggap huruf)
+ * 4. Semua nama variable adalah case-sensitive (yang berarti x dan X itu berbeda)
+ * 5. `Reserved words` (seperti kata kunci var, do, dll) tidak dapat digunakan sebagai nama variable
+ * - untuk melihat lengkap daftar `Reserved words` cek link berikut: https://mathiasbynens.be/notes/reserved-keywords
+ */
+
+//  var 7name = 7; // ini salah
+var name7 = "sakuke";
+
+var _name = "narji";
+var $age = 24;
+
+var car = "Mobilio";
+var Car = "Avanza";
+var cAr = "asasas";
+
+// var do = 2; ini tidak boleh
+
+console.log($age);
+
+// cara 1 : menggunakan var
+var mySchool = "SMK";
+
+// cara 2 : menggunakan let
+let myGirl = "Sinta dan Jojo";
+myGirl = "Danish";
+
+// cara 3 : menggunakan const
+const phi = 3.14;
